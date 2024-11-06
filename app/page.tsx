78fc6b18949a3132f78717109538e6e9ab2c98e5
Daniel Azara <danielazara04@gmail.com>
2024-11-05 23:04:04 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div className="jus flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Hello World</h1>
-    </div>
-  );
-}
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
+  return <Button>Hello</Button>;
+};
+
+export default Home;
